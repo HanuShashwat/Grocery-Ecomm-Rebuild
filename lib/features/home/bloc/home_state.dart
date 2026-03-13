a889fb17a494d17fbe08d63451c2099f406d0deb
Hanu Shashwat <hanushashwat733@gmail.com>
2026-03-13 23:56:10 +0530

    Defined BLoC states for Home page.

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -6,3 +6,13 @@ sealed class HomeState {}
 sealed class HomeActionState extends HomeState {}
 
 class HomeInitial extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeSuccessState extends HomeState {}
+
+class HomeErrorState extends HomeState {}
+
+class HomeNavigateToWishlistState extends HomeActionState {}
+
+class HomeNavigateToCartState extends HomeActionState {}
