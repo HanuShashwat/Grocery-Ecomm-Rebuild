14f709f3e6c7948173e71b50b3f1de3e19320fc3
Hanu Shashwat <hanushashwat733@gmail.com>
2026-03-17 22:37:42 +0530

    feat(wishlist_page.dart): add app bar to wishlist page with custom styling

--- lib/features/wishlist/ui/wishlist_page.dart
+++ lib/features/wishlist/ui/wishlist_page.dart
@@ -10,6 +10,17 @@ class WishlistPage extends StatefulWidget {
 class _WishlistPageState extends State<WishlistPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Wishlist Page')));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        iconTheme: IconThemeData(color: Colors.white),
+        titleSpacing: -4,
+        title: Text(
+          'Wishlist Page',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Center(child: Text('Wishlist Page')),
+    );
   }
 }
